67acba710cc557a6eca33c0d489f1156bca19b2e
Prob4bly Anon <76247497+prob4bly-anon@users.noreply.github.com>
2023-01-18 12:25:20 +0530

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,5 @@
+(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+//^^^ Eruda :)
 
 function openTab(event, tabId) {
  var tabContent = document.getElementsByClassName("tabcontent");
